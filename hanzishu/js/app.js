775f70dcf3b7aa767599949b2ea62bc24130054b
chineseclassics <gnoluy@gmail.com>
2025-11-06 21:38:20 +0800

    Remove iframe-specific optimizations and decorations
    Deleted iframe.js utility and related CSS classes for iframe optimization. Removed the import and usage of optimizeForIframe in app.js. Also removed the .seal-decoration CSS and associated responsive styles.

--- hanzishu/js/app.js
+++ hanzishu/js/app.js
@@ -1,5 +1,3 @@
-import { optimizeForIframe } from './utils/iframe.js';
 import { initializeApp } from './main.js';
 
-optimizeForIframe();
 document.addEventListener('DOMContentLoaded', initializeApp);
